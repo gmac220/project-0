989d6c4b174686b30310892ea012cddcdb15d1e0
gmac220 <godfreymacasero@gmail.com>
2019-10-07 01:17:58 -0500

    Made account struct and account test file

---
+++ account.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type Account struct {
+	accountname string
+	balance     float64
+}
+
+// Account constructor
+func NewAccount(name string, money float64) *Account {
+	return &Account{
+		accountname: name,
+		balance:     money,
+	}
+}
+
+// Balance returns the current balance
+func (a *Account) Balance() float64 {
+	return a.balance
+}
+
+// Withdraw money from the bank
+func (a *Account) Withdraw(money float64) {
+	if a.balance-money >= 0 {
+		a.balance -= money
+	} else {
+		fmt.Println("Not enough money to withdraw")
+	}
+}
+
+// Deposit money to balance
+func (a *Account) Deposit(money float64) {
+	if money > 0 {
+		a.balance += money
+	} else {
+		fmt.Println("Please enter a positive integer")
+	}
+}
