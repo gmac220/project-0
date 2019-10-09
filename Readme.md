2eb232a0b2d63fe80e64c7f9d84880fc446e8ba0
gmac220 <godfreymacasero@gmail.com>
2019-10-09 14:36:07 -0500

    working apply and apply for joint

--- Readme.md
+++ Readme.md
@@ -13,7 +13,9 @@ This app simulates banking on the terminal with CLI like commands that allows ba
 - [] Employees should be able to:
     - [] View customer information
     - [] Approve/deny open applications for accounts
-- [] All account and user information should persist to files or a database
+    - [x] Register with a username and password
+    - [x] Authenticate (login) with that usename and password
+- [x] All account and user information should persist to files or a database
 - [] Usable from command line args, interactive text menus, or through HTTP
 - [] Basic validation and testing
 
@@ -25,8 +27,8 @@ go get -u github.com/lib/pq
 Inside project the database folder where Dockerfile is run
 ```bash
 cd db
-sudo docker build -t billsfargo .
-sudo docker image ls
-sudo docker run -p 5432:5432 -d --rm --name runningbillsfargo billsfargo
-sudo docker exec -it runningbillsfargo psql -U postgres
+docker build -t billsfargo .
+docker image ls
+docker run -p 5432:5432 -d --rm --name runningbillsfargo billsfargo
+docker exec -it runningbillsfargo psql -U postgres
 ```
--- customer.go
+++ customer.go
@@ -46,11 +46,11 @@ func CustomerPage(username string, firstname string, lastname string) {
 	case 1:
 		Apply(username, firstname, lastname, false)
 	case 2:
-		JointApp()
+		JointApp(username, firstname, lastname, true)
 	case 3:
-		ShowAccounts()
+		ShowAccounts(username)
 	case 4:
-		ShowBalance()
+		ShowBalance(username)
 	case 5:
 		Withdraw()
 	case 6:
@@ -67,10 +67,11 @@ func CustomerPage(username string, firstname string, lastname string) {
 // Apply adds to applications table
 func Apply(username string, firstname string, lastname string, joint bool) {
 	//Adds data to application table on database
-	db := OpenDB()
-	defer db.Close()
 	var acntname string
 	var appcount int
+
+	db := OpenDB()
+	defer db.Close()
 	fmt.Printf("What type of account do you want to open? checking or savings: ")
 	fmt.Scanln(&acntname)
 	db.Exec("INSERT INTO applications (username, firstname, lastname, acntname, joint)"+
@@ -81,39 +82,95 @@ func Apply(username string, firstname string, lastname string, joint bool) {
 }
 
 // JointApp adds to applications table
-func JointApp() {
-	//Adds data to application table on database
+func JointApp(username string, firstname string, lastname string, joint bool) {
+	var uname2 string
+	var fname2 string
+	var lname2 string
+	var acntname string
 
+	db := OpenDB()
+	defer db.Close()
+	fmt.Printf("What type of account do you want to open? checking or savings: ")
+	fmt.Scanln(&acntname)
+	fmt.Printf("What user do you want to share an account with? Please input username of that user: ")
+	fmt.Scanln(&uname2)
+	uname2, fname2, lname2 = CheckCustomer(uname2)
+	for uname2 == "" {
+		fmt.Printf("This user does not exist please input a valid user: ")
+		fmt.Scanln(&uname2)
+		uname2, fname2, lname2 = CheckCustomer(uname2)
+	}
+
+	db.Exec("INSERT INTO applications"+
+		"(username, firstname, lastname, acntname, joint, username2, firstname2, lastname2)"+
+		"VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+		username, firstname, lastname, acntname, true, uname2, fname2, lname2)
+}
+
+// CheckCustomer verifies if the customer exists
+func CheckCustomer(username string) (string, string, string) {
+	var uname2 string
+	var fname2 string
+	var lname2 string
+	var appcnt int
+	var pw string
+
+	db := OpenDB()
+	defer db.Close()
+	row := db.QueryRow("SELECT * FROM customers WHERE username = $1", username)
+	row.Scan(&uname2, &pw, &fname2, &lname2, &appcnt)
+
+	return uname2, fname2, lname2
 }
 
 // ShowAccounts lists out the accounts the user currently has
-func ShowAccounts() {
-
+func ShowAccounts(username string) {
+	db := OpenDB()
+	defer db.Close()
+	//loop through both accounts and joint accounts
+	rows, _ := db.Query("SELECT * FROM accounts WHERE username = ")
+	for rows.Next() {
+		var id int
+		var name string
+		rows.Scan(&id, &name)
+		fmt.Println(id, name)
+	}
 }
 
 // ShowBalance of account
-func ShowBalance() {
+func ShowBalance(username string) {
+	var account string
+	var balance float64
+	var acntnumber int
+	var uname string
+
 	fmt.Println("What account do you want to check the balance for?: ")
+	fmt.Scanln(&account)
+	db := OpenDB()
+	defer db.Close()
+	row := db.QueryRow("SELECT balance FROM accounts WHERE acntname = $1", account)
+	row.Scan(&acntnumber, &account, &balance, &uname)
+	fmt.Println("This is your balance: ", balance)
 
 }
 
 // Withdraw money to bank account balance
-func Withdraw() {
+func Withdraw(username string) {
 
 }
 
 // Deposit money to bank account balance
-func Deposit() {
+func Deposit(username string) {
 
 }
 
 // ShowPendingApps shows the amount of applications the user has applied to
-func ShowPendingApps() {
+func ShowPendingApps(username string) {
 
 }
 
 // Transfer money from one account to the other
-func Transfer() {
+func Transfer(username string) {
 	// account1 := c.accounts[acntname]
 	// account2 := c.accounts[acntname2]
 	// account1.Withdraw(money)
